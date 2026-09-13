bf604c3b9bb6d6f6ec2fcfeeb8d94ba22cc0b7e2
Valentino Alfred <drivenbydomain.com>
2026-09-13 16:07:29 +0100

    Import ProfileUpdateForm in views.py

--- club/views.py
+++ club/views.py
@@ -1,5 +1,6 @@
 import json
 import time
+from .forms import ProfileUpdateForm
 from functools import wraps
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
